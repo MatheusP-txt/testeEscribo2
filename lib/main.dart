ac3b0190595fad734cb8019813ff84e0bf56e1d2
MatheusP-txt <matheusp.txt@gmail.com>
2023-11-23 03:25:46 -0300

    feat:create meus livros

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,25 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text("Leitor de eBooks"),
       ),
-      body: ,
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Meus Livros",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              SizedBox(height: 12.0
+              
+              ,)
+            ]),
+      )),
     );
   }
 }
