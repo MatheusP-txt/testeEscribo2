852957f99fd03a07dfb0d14f930a8e15df3d1357
MatheusP-txt <matheusp.txt@gmail.com>
2023-11-23 03:30:11 -0300

    feat:create bookdata

---
+++ lib/data/bookdata.dart
@@ -0,0 +1,16 @@
+class BookData {
+  String id;
+  String bookCover;
+  String bookTitle;
+  String author;
+  String downloadUrl;
+
+  BookData({
+    required this.id,
+    required this.bookCover,
+    required this.bookTitle,
+    required this.author,
+    required this.downloadUrl,
+  });
+
+}
